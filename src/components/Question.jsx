ac07cd07cd5c035a58021bc6e015ae9f12edf040
Mike Mylonakis <mike.mylonakis@transficc.com>
2024-12-09 12:35:11 +0000

    Style the images

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,25 +1,23 @@
-import { useEffect, useRef } from 'react';
 import Options from './Options';
 
 function Question({ index, question, dispatch, answer }) {
-	const imgRef = useRef(null);
-	const h4Ref = useRef(null);
-
-	useEffect(() => {
-		if (imgRef.current && h4Ref.current) {
-			imgRef.current.style.width = `${h4Ref.current.offsetWidth}px`;
-		}
-	}, [question]);
-
 	return (
 		<div>
-			<div>
-				<img ref={imgRef} src={`${index}.jpeg`} alt={`Image ${index}`} style={{ height: 'auto' }} />
+			<div style={{ width: '100%', maxHeight: '50vh', display: 'flex', justifyContent: 'center', marginBottom: '2rem' }}>
+				<img
+					src={`${index}.jpeg`}
+					alt={`Image ${index}`}
+					style={{
+						width: '100%',
+						height: '100%',
+						maxHeight: '50vh',
+						objectFit: 'contain'
+					}}
+				/>
 			</div>
-			<h4 ref={h4Ref}>{question.question}</h4>
+			<h4>{question.question}</h4>
 			<Options question={question} dispatch={dispatch} answer={answer} />
 		</div>
 	);
 }
-
 export default Question;
