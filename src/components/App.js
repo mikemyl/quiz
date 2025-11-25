ff36520d8c2fea71776b870e7f83cb4c48d5b881
Mike Mylonakis <mike.mylonakis@transficc.com>
2025-11-25 14:50:03 +0000

    random facts quiz

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ import Footer from './Footer';
 import Timer from './Timer';
 import questions from '../data/questions.json';
 
-const SECS_PER_QUESTION = 30;
+const SECS_PER_QUESTION = 90;
 
 const initialState = {
 	questions: questions,
